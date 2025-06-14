0dd32b5c623437ba92ca2b660bef1c2c64880a65
Saksham Tomer <132002655+saksham-tomer@users.noreply.github.com>
2025-06-15 05:09:27 +0530

    added drop view

--- components/Sidebar/DropView.tsx
+++ components/Sidebar/DropView.tsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-type Props = {}
+type Props = {
+  title: string,
+  chats: Array<string>
+  setSelectedPinned?: () => void
+}
+
+declare enum STRING_SIZE {
+  SIZE = 24
+}
 
 const DropView = (props: Props) => {
+
+  const [pinned, setPinned] = useState<number | null>(null);
+
+
   return (
-    <div>DropView</div>
+    <div className='flex flex-col items-center relative min-h-full'>
+      <div className='min-w-full min-h-full bg-gradient-to-b from-white/0 to-white/70 absolute inset-0' />
+      <title className='uppercase text-gray-400 font-semibold text-sm mt-4 mb-4'>{props.title}</title>
+      <div className="flex flex-col gap-4 items-center text-start font-medium text-sm text-gray-600">
+        {
+          props.chats.map((chat: any, idx: number) => (
+            <p>{chat.substring(0, STRING_SIZE.SIZE)}...</p>
+          ))
+        }
+      </div>
+    </div>
   )
 }
 
--- components/Sidebar/SidebarView.tsx
+++ components/Sidebar/SidebarView.tsx
@@ -4,8 +4,24 @@ import PortAILogo from '@/public/PortAi.png'
 import { PanelRight, Plus, Settings } from "lucide-react";
 import ChatSearch from "./ChatSearch";
 import Tabs from "./Tabs";
+import DropView from "./DropView";
+
+const messages = [
+    "The zebra danced through the thunderstorm like a rockstar.",
+    "Quantum muffins are illegal in seventeen galaxies.",
+    "He wore confidence like a second-hand coat in spring.",
+    "A cactus once told me its dreams in Morse code.",
+    "Yesterday’s spaghetti rewrote history in the microwave.",
+    "Banana phones only work during full moons.",
+    "The librarian screamed in binary before vanishing.",
+    "Time travel is just poorly managed memory allocation.",
+    "Clouds argued over who would rain first.",
+    "My thoughts are encrypted in pineapple salsa."
+];
+
 
 export default function SidebarView({ children }: { children: React.ReactNode }) {
+    document.title = "PortAI"
     return (
         <nav className="flex bg-[var(--gray)] flex-col font-[var(--font-inter)] min-h-screen  max-w-[20rem] items-center p-4 lg:p-6">
             <section className="flex flex-row items-center justify-between w-full">
@@ -26,10 +42,18 @@ export default function SidebarView({ children }: { children: React.ReactNode })
                 <ChatSearch />
             </section>
             <section className="w-full mt-12">
-                <Tabs currentTab={0}/>
+                <Tabs currentTab={0} />
             </section>
+
+            <section className="w-full mt-12">
+                <DropView title="pinned" chats={messages} />
+            </section>
+            <section className="w-full mt-12">
+                <DropView title="chat history" chats={messages} />
+            </section>
+
             <button className="p-2 text-white py-3 text-sm gap-2 bg-gradient-to-b from-[#4C29FD] to-[#6927FF] w-full mt-4 rounded-xl hover:bg-[#6927FF] transition-colors duration-200 ease-in-out cursor-pointer text-center flex flex-row items-center justify-center">
-                <Plus className="w-4 h-4 text-white"/>
+                <Plus className="w-4 h-4 text-white" />
                 Start new chat
             </button>
             <section className="w-full mt-4">
